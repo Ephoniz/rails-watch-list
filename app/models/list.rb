f72596ea0d1dc36ccf388d8bbcb9c41f2bc29757
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-03 17:46:44 +0200

    Added destroy functions, and different images for each list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 
+  validates :image_url, presence: true
   validates :name, presence: true, uniqueness: true
 end
