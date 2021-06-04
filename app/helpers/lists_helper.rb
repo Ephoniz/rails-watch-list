2fe280bd16ba63f65367a3ce4670be299e25ac6a
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-04 14:47:49 +0200

    Added no image

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,9 @@
 module ListsHelper
+  def list_image(list)
+    if list.thumbnail.attached?
+      cl_image_path(list.thumbnail.key)
+    else
+      'https://barcelona.lecool.com/files/2016/02/no-image-available.jpg'
+    end
+  end
 end
