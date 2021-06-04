6eedeb5c7d4b2ca7cdf0cfe216c6d1b101df63ca
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-04 12:08:32 +0200

    fixed issues

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,7 +17,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :bookmarks, only: %i[new create destroy]
+  root to: 'lists#index'
+
+  resources :lists, only: %i[new create show index] do
+    resources :bookmarks, only: %i[new create]
   end
+
+  resources :bookmarks, only: :destroy
+  resources :lists, only: :destroy
 end
